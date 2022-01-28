925bcdb1a70476a382b6c524d5b6ee06e5fc7c9c
PogorelovaTeacher <pogorelova_marin@mail.ru>
2022-01-28 17:04:35 +0300

    first commit

---
+++ SolutionMultyProject/ProjectOne/Calendar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace ProjectOne
+{
+    public class Calendar
+    {
+        public static void Main()
+        {
+            DateTime now = GetCurrentDate();
+            Console.WriteLine($"Today's date is {now} ");
+            //MessageBox.Show($"Today's date is {now} ");
+            Console.ReadLine();
+
+            
+        }
+
+        static DateTime GetCurrentDate()
+        {
+            return DateTime.Now.Date;
+        }
+
+
+    }
+}
---
+++ SolutionMultyProject/ProjectTwo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("ProjectTwo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AlexSoft")]
+[assembly: AssemblyProduct("ProjectTwo")]
+[assembly: AssemblyCopyright("Copyright © AlexSoft 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("58071410-a874-4924-a07e-add02752d6ae")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
